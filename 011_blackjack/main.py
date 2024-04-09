1ff63b895378d039b9a37f931d7d9b7c3d25e335
firletta <firletta@gmail.com>
2024-04-09 17:29:49 +0200

    added repetition of game

--- 011_blackjack/main.py
+++ 011_blackjack/main.py
@@ -10,42 +10,46 @@ RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 deck = [rank + suit for suit in SUITS for rank in RANKS]
 
 def main():
+    while True:
+        print(logo)
+        
+        player_cards = [deal_card(), deal_card()]
+        computer_cards = [deal_card(), deal_card()]
 
-    print(logo)
-    
-    player_cards = [deal_card(), deal_card()]
-    computer_cards = [deal_card(), deal_card()]
+        game_over = False
 
-    game_over = False
+        while not game_over:
+            player_score = calculate_score(player_cards)
+            computer_score = calculate_score(computer_cards)
+            print(f"Your cards: {format_cards(player_cards)}\nCurrent score: {player_score}\n\nComputer's first card: {'|' + computer_cards[0] + '|'}\n")
 
-    while not game_over:
-        player_score = calculate_score(player_cards)
-        computer_score = calculate_score(computer_cards)
-        print(f"Your cards: {format_cards(player_cards)}\nCurrent score: {player_score}\n\nComputer's first card: {'|' + computer_cards[0] + '|'}\n")
-
-        if player_score == 21 or computer_score == 21 or player_score > 21:
-            game_over = True
-        else:
-            should_continue = input("Type 'hit' to get another card, 'stand' to pass:\n").lower().strip(" ")
-            if should_continue == 'hit':
-                player_cards.append(deal_card())
-            else:
+            if player_score == 21 or computer_score == 21 or player_score > 21:
                 game_over = True
+            else:
+                should_continue = input("Type 'hit' to get another card, 'stand' to pass:\n").lower().strip(" ")
+                if should_continue == 'hit':
+                    player_cards.append(deal_card())
+                else:
+                    game_over = True
 
-    while computer_score != 21 and computer_score < 17:
-        computer_cards.append(deal_card())
-        computer_score = calculate_score(computer_cards)
+        while computer_score != 21 and computer_score < 17:
+            computer_cards.append(deal_card())
+            computer_score = calculate_score(computer_cards)
 
-    print(f"\nYour final hand: {format_cards(player_cards)}\nFinal score: {player_score}\n\nComputer's final hand: {format_cards(computer_cards)}\nFinal score: {computer_score}")
+        print(f"\nYour final hand: {format_cards(player_cards)}\nFinal score: {player_score}\n\nComputer's final hand: {format_cards(computer_cards)}\nFinal score: {computer_score}")
 
-    if player_score > 21 or (computer_score <= 21 and computer_score > player_score):
-        result = "\nYou lose."
-    elif player_score == computer_score:
-        result = "\nIt's a draw."
-    else:
-        result = "\nYou win!"
+        if player_score > 21 or (computer_score <= 21 and computer_score > player_score):
+            result = "\nYou lose."
+        elif player_score == computer_score:
+            result = "\nIt's a draw."
+        else:
+            result = "\nYou win!"
 
-    print(result)
+        print(result)
+
+        play_again = input("\nDo you want to play again? Type 'y' for yes, 'n' for no:\n").lower().strip(" ")
+        if play_again != 'y':
+            break
 
 
 
