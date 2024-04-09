52d3b717fbe8fcab160b688252c0f54505e5e50c
firletta <firletta@gmail.com>
2024-04-09 22:01:11 +0200

    naming

--- 012_guess_the_number/main.py
+++ 012_guess_the_number/main.py
@@ -7,15 +7,15 @@ ATTEMPS_HARD = 5
 
 def main():
     print(f"{logo}\nWelcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
-    random_number = random.randint(1, MAX_NUMBER)
+    answer = random.randint(1, MAX_NUMBER)
     attempts = ATTEMPS_EASY if input("Choose a difficulty. Type 'easy' or 'hard': ") == "easy" else ATTEMPS_HARD
 
     while attempts > 0:
         user_guess = int(input(f"\nYou have {attempts} attempts remaining to guess the number.\nMake a guess: "))
 
-        if user_guess < random_number:
+        if user_guess < answer:
             print("Too low.")
-        elif user_guess > random_number:
+        elif user_guess > answer:
             print("Too high.")
         else:
             print("You got it!")
@@ -26,7 +26,7 @@ def main():
         if attempts > 0:
             print("Guess again.")
         else:
-            print(f"You've run out of guesses, you lose.\nThe number was {random_number}.")
+            print(f"You've run out of guesses, you lose.\nThe number was {answer}.")
             break
   
 if __name__ == "__main__":
